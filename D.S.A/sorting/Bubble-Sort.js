e5b17df0be70385e910327eaddf7f631f253ba44
Pranav <boyp56039@gmail.com>
2026-02-02 17:13:59 +0530

    Bubble-Sort

---
+++ D.S.A/sorting/Bubble-Sort.js
@@ -0,0 +1,30 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+}
+
+console.log(bubbleSort([23, 1, 34, 11, 10]));
+
+//Bubble Sort repeatedly compares adjacent elements and swaps them if they are in the wrong order, gradually pushing the largest elements to the end of the array.
+
+// Time & Space Complexity
+
+// Time Complexity:
+
+// Best: O(n) (with optimization)
+
+// Average & Worst: O(n²)
+
+// Space Complexity: O(1)
+
