33c822a4f353160de3aa5ace330e170ff9b128c9
Pranav <boyp56039@gmail.com>
2026-02-08 19:07:37 +0530

    c++ basics

---
+++ C++/sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,sum;
+
+    cout<<"Enter a number"<<endl;
+    cin>>a;
+
+    cout<<"Enter a number"<<endl;
+    cin>>b;
+
+    sum=a+b;
+
+    cout<<"Sum is "<< sum<< endl;
+
+    return 0;
+}
