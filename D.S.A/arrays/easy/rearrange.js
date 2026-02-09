2c8741a1c935104c73783209534984336d132a06
Pranav <boyp56039@gmail.com>
2026-02-09 15:43:50 +0530

    rearrange-elements. optimal

--- D.S.A/arrays/easy/rearrange.js
+++ D.S.A/arrays/easy/rearrange.js
@@ -1,27 +1,47 @@
 
 //brute force
-function rearrange(arr){
+// function rearrange(arr){
    
-  let pos=[];
-  let neg=[];
+//   let pos=[];
+//   let neg=[];
 
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]>0){
-        pos.push(arr[i])
-    }else{
-        neg.push(arr[i]);
-    }
-  }
+//   for(let i=0;i<arr.length;i++){
+//     if(arr[i]>0){
+//         pos.push(arr[i])
+//     }else{
+//         neg.push(arr[i]);
+//     }
+//   }
 
 
  
 
-  for(let i=0;i<Math.floor(arr.length/2);i++){
-      arr[2*i]=pos[i];
-      arr[2*i+1]=neg[i];
-  }
+//   for(let i=0;i<Math.floor(arr.length/2);i++){
+//       arr[2*i]=pos[i];
+//       arr[2*i+1]=neg[i];
+//   }
 
-  console.log(arr);
-}
+//   console.log(arr);
+// }
 
-rearrange([3,1,-2,-5,2,-4]);
+// rearrange([3,1,-2,-5,2,-4]);
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    let posindex=0,negindex=1;
+    let ans=[]
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>0){
+            ans[posindex]=nums[i];
+            posindex+=2;
+        }else{
+            ans[negindex]=nums[i];
+            negindex+=2; 
+        }
+    }
+    return ans;
+};
