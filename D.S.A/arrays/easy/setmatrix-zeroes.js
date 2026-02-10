cebb85f622b430157d93c4bfb11b1a43b5d2bfc3
Pranav <boyp56039@gmail.com>
2026-02-10 16:05:12 +0530

    setmetrixzeroes

---
+++ D.S.A/arrays/easy/setmatrix-zeroes.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+   let rowslength= matrix.length;
+    let collength=matrix[0].length;
+
+    let zerorows=new Array(rowslength);
+    let zerocols=new Array(collength);
+
+    for(let i=0;i<rowslength;i++){
+    
+        for(let j=0;j<collength;j++){
+            if(matrix[i][j]===0){
+             zerorows[i]=1;
+             zerocols[j]=1;
+            }
+        }
+    }
+
+    for(let k=0;k<rowslength;k++){
+        if(zerorows[k]===1){
+            let i=k;
+           for(let j=0;j<collength;j++){
+            matrix[i][j]=0;
+           }
+        }
+    }
+
+     for(let k=0;k<collength;k++){
+        if(zerocols[k]===1){
+            let j=k;
+           for(let i=0;i<rowslength;i++){
+            matrix[i][j]=0;
+           }
+        }
+    }
+
+
+};
