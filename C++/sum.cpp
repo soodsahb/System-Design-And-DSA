44cc0d5f6f87cc528cdc0a25e63ee824f16f853b
Pranav <boyp56039@gmail.com>
2026-02-12 14:47:25 +0530

    variables in c++

--- C++/sum.cpp
+++ C++/sum.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main(){
 
-    int a,b,sum;
+   int a,b,sum;
 
-    cout<<"Enter a number"<<endl;
-    cin>>a;
+   cout<<"Enter a number"<<endl;
+   cin>>a;
 
-    cout<<"Enter a number"<<endl;
-    cin>>b;
+   cout<<"Enter second number"<<endl;
+   cin>>b;
 
-    sum=a+b;
+   sum=a+b;
 
-    cout<<"Sum is "<< sum<< endl;
+   cout<<"Sum is "<<sum<<endl;
 
     return 0;
 }
---
+++ C++/variables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+int age=10;
+float marks=3.5;
+char grade='A';
+bool isHappy=true;
+string name="Pranav";
+
+cout<<name<<" is "<<age<<" years old"<<endl;
+
+}
+
