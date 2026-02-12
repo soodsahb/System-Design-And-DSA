2501c816197b361fc0223e001a0dd49c5b188b8d
Pranav <boyp56039@gmail.com>
2026-02-12 15:02:01 +0530

    iflese

---
+++ C++/if-else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cin>>a;
+
+    if(a<0){
+   
+        cout<<"Negative"<<endl;
+    }else if(a==0){
+        cout<<"Zero"<<endl;
+    }
+    else{
+        
+        cout<<"Postive"<<endl;
+    }
+
+
+}
