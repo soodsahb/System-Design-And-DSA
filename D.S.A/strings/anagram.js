26b78bda1fe413d6d9ccfd2d6f9793d7f0956e24
Pranav <boyp56039@gmail.com>
2026-02-13 12:54:46 +0530

    Anagram string

---
+++ D.S.A/strings/anagram.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let map = new Map();
+
+    if(s.length!==t.length) return false;
+
+    for(let i=0;i<s.length;i++){
+        if(map.has(s[i])){
+            map.set(s[i],map.get(s[i])+1);
+        }else{
+            map.set(s[i],1);
+        }
+    }
+
+    for(let i=0;i<t.length;i++){
+        if(map.has(t[i])){
+         map.set(t[i],map.get(t[i])-1)
+        }else{
+            return false;
+        }
+    }
+
+    for(let value of map.values()){
+     
+     if(value!==0){
+        return false;
+     }
+    }
+
+    return true;
+};
