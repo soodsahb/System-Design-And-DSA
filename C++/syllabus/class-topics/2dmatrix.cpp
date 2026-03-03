02c37ae5ba6a9e86acd76f170ea02fd36888814b
Pranav <boyp56039@gmail.com>
2026-03-03 12:32:15 +0530

    2 matrix cpp

---
+++ C++/syllabus/class-topics/2dmatrix.cpp
@@ -0,0 +1,35 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+void acceptdata(int** p,int rows, int cols){
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            cin>>p[i][j];
+        }
+    }
+}
+
+void displaydata(int**p, int rows,int cols){
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            cout<<p[i][j]<<" ";
+        }
+    }
+}
+
+int main() {
+    // Write C++ code here
+  int **p = new int*[3];
+  int rows=3,cols=3;
+  
+  for(int i=0;i<rows;i++){
+      p[i]=new int[cols];
+  }
+  acceptdata(p,rows,cols);
+  displaydata(p,rows,cols);
+  
+
+    return 0;
+}
