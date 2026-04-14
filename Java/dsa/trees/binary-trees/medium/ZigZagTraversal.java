90744d23640cf29386aa0b3df1f2fef5a164853a
Pranav <boyp56039@gmail.com>
2026-04-14 19:11:54 +0530

    zigzag-traversal

---
+++ Java/dsa/trees/binary-trees/medium/ZigZagTraversal.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+       
+       
+        List<List<Integer>> result= new ArrayList<>();
+        if(root==null) return result;
+
+        boolean leftToRight= true;
+        Queue<TreeNode> q= new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+           
+              int size = q.size();
+             List<Integer> level= new LinkedList<>();
+         
+            for(int i=0;i<size;i++){
+                 
+
+            TreeNode tmp=q.poll();
+           
+            if(leftToRight){
+                level.addLast(tmp.val);
+            }else{
+                level.addFirst(tmp.val);
+            }
+
+            if(tmp.left!=null) q.add(tmp.left);
+            if(tmp.right!=null) q.add(tmp.right);
+
+         
+
+           
+
+            }
+              result.add(level);
+               leftToRight=!leftToRight;
+           
+        }
+
+        return result;
+    }
+}
